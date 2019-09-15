07a9ff837e836ce74746e5b831b1ded0a552da5c
Alex Uvi <yauvarov@yandex.ru>
2019-09-16 00:00:25 +0300

    Finding the minimum element in an array

--- src/Task.java
+++ src/Task.java
@@ -1,25 +1,66 @@
 import java.util.Arrays;
 
+/**
+ *Finding the minimum element in an array
+ */
+
+//public class Task{
+//    public static void main(String[] args) {
+//        int[] arr = new int[] {64, 42, 73, 41, 31, 53, 16, 24, 57, 42, 74, 55, 36};
+//
+//        int minValue = arr[0];
+//        int minIndex = 0;
+//
+//        for (int i = 1; i < arr.length; i++){
+//            if(arr[i] < minValue){
+//                minValue = arr[i];
+//                minIndex = i;
+//            }
+//        }
+//
+//        System.out.println("min value: " + minValue);
+//        System.out.println("min index: " + minIndex);
+//    }
+//}
+
+/**
+ * Linear Search
+ */
+
+//public class Task {
+//
+//    public static void main(String[] args) {
+//        int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+//        System.out.println(Arrays.toString(array));
+//
+//        System.out.println(searchLinear(array, 10));
+//    }
+//
+//    public static int searchLinear(int[] array, int elementToFind) {
+//        for (int i = 0; i < array.length; i++) {
+//            System.out.println("searhing at index: " + i + ", value: " + array[i]);
+//            if (array[i] == elementToFind) {
+//                System.out.println("found at index: " + i + ", value: " + array[i]);
+//                return i;
+//            }
+//        }
+//        System.out.println("not found: " + elementToFind);
+//        return -1;
+//    }
+//}
+
+/**
+ * Binary Searching
+ */
+
 public class Task {
 
-    public static void main(String[] params) {
-        int[] array = new int[] {64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
+    public static void main(String[] args) {
+        int[] array = new int[]{64, 42, 73, 41, 32, 53, 16, 24, 57, 42, 74, 55, 36};
         bubbleSort(array);
-        System.out.println(arrayToString(array));
-
+        System.out.println(Arrays.toString(array));
         System.out.println(searchBinary(array, 42));
-    }
-
-    public static int searchLinear(int[] array, int elementToFind) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.println("searhing at index: " + i + ", value: " + array[i]);
-            if (array[i] == elementToFind) {
-                System.out.println("found at index: " + i + ", value: " + array[i]);
-                return i;
-            }
-        }
-        System.out.println("not found: " + elementToFind);
-        return -1;
+//        System.out.println(searchBinaryRecursive(array, 0,array.length - 1, 42));
     }
 
     public static int searchBinary(int[] array, int elementToFind) {
@@ -44,52 +85,35 @@ public class Task {
         return -1;
     }
 
-    public static int searchBinaryRecursive(int array[], int startIndex, int endIndex, int elementToFind) {
-        if (endIndex >= startIndex) {
-            int middleIndex = startIndex + (endIndex - startIndex) / 2;
-
-            if (array[middleIndex] == elementToFind) {
-                return middleIndex;
-            }
-
-            if (array[middleIndex] > elementToFind) {
-                return searchBinaryRecursive(array, startIndex, middleIndex - 1, elementToFind);
-            } else {
-                return searchBinaryRecursive(array, middleIndex + 1, endIndex, elementToFind);
-            }
-        }
-
-        return -1;
-    }
-
-    //System.out.println("startIndex: " + startIndex + ", endIndex: " + endIndex + ", middleIndex: " + middleIndex);
+//    public static int searchBinaryRecursive(int array[], int startIndex, int endIndex, int elementToFind) {
+//        if (endIndex >= startIndex) {
+//            int middleIndex = startIndex + (endIndex - startIndex) / 2;
+//
+//            if (array[middleIndex] == elementToFind) {
+//                return middleIndex;
+//            }
+//
+//            if (array[middleIndex] > elementToFind) {
+//                return searchBinaryRecursive(array, startIndex, middleIndex - 1, elementToFind);
+//            } else {
+//                return searchBinaryRecursive(array, middleIndex + 1, endIndex, elementToFind);
+//            }
+//        }
+//        return -1;
+//    }
 
     public static void bubbleSort(int[] array) {
         boolean isSorted = false;
         while (!isSorted) {
             isSorted = true;
             for (int i = 1; i < array.length; i++) {
-                if (array[i - 1] > array[i]) {
-                    int tmp = array[i];
+                if (array[i] < array[i - 1]) {
+                    int temp = array[i];
                     array[i] = array[i - 1];
-                    array[i - 1] = tmp;
+                    array[i - 1] = temp;
                     isSorted = false;
                 }
             }
         }
     }
-
-    private static String arrayToString(int[] array) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("[");
-        for (int i = 0; i < array.length; i++) {
-            if (i > 0) {
-                sb.append(", ");
-            }
-            sb.append(array[i]);
-        }
-        sb.append("]");
-        return sb.toString();
-    }
-
 }
